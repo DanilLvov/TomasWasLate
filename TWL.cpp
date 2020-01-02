a14cbd0247398503e7d898bc1e26c9c6856694b9
DanilLvov <danillvov2002@gmail.com>
2020-01-02 14:41:44 +0200

    test commit

---
+++ TWL.cpp
@@ -0,0 +1,12 @@
+// Include important C++ libraries here
+#include "stdafx.h"
+#include<SFML/Graphics.hpp>
+
+using namespace sf;
+int main()
+{
+	VideoMode vm(1366, 720);
+	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+	return 0;
+}
+
