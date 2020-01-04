7d9b3763b8ff5809f0bd26f04793b1d6efb429ba
DanilLvov <danillvov2002@gmail.com>
2020-01-04 12:00:25 +0200

    Commit 4: Finished Engine class

--- Engine.cpp
+++ Engine.cpp
@@ -1,8 +1,55 @@
 #include "stdafx.h"
 #include "Engine.h"
 
-
 Engine::Engine()
 {
+	// creating window and views
+	Vector2f resolution;
+	resolution.x = VideoMode::getDesktopMode().width;
+	resolution.y = VideoMode::getDesktopMode().height;
+
+	VideoMode vm(resolution.x, resolution.y);
+	m_Window.create(vm, "TWL", Style::Fullscreen);
+
+	// Initialize the main view
+	m_MainView.setSize(resolution);
+	m_HudView.reset(
+		FloatRect(0, 0, resolution.x, resolution.y));
+
+	// Initialize the split-screen Views
+	m_LeftView.setViewport(
+		FloatRect(0.001f, 0.001f, 0.498f, 0.998f));
+	m_RightView.setViewport(
+		FloatRect(0.5f, 0.001f, 0.499f, 0.998f));
+	m_BGLeftView.setViewport(
+		FloatRect(0.001f, 0.001f, 0.498f, 0.998f));
+	m_BGRightView.setViewport(
+		FloatRect(0.5f, 0.001f, 0.499f, 0.998f));
+
+	// load BG texture
+	m_BGTexture = TextureHolder::GetTexture("graphics/background.png");
+
+	// Associate the sprite with the texture
+	m_BGSprite.setTexture(m_BGTexture);
+}
+
+void Engine::run()
+{
+	Clock clock;
+
+	while (m_Window.isOpen())
+	{
+		Time dt = clock.restart();
+
+		// Update the total game time
+		m_GameTimeTotal += dt;
+
+		// Make a decimal fraction from the delta time
+		float dtAsSeconds = dt.asSeconds();
+
+		m_input();
+		m_update(dtAsSeconds);
+		m_draw();
+	}
 }
 
--- Engine.h
+++ Engine.h
@@ -2,25 +2,30 @@
 
 #include "TextureHolder.h"
 
+using namespace sf;
+
 class Engine
 {
 // private class objects
 private:
 	TextureHolder holder;
-	sf::RenderWindow m_Window;
+	RenderWindow m_Window;
 
 	// Main views
-	sf::View m_MainView;
-	sf::View m_LeftView;
-	sf::View m_RightView;
+	View m_MainView;
+	View m_LeftView;
+	View m_RightView;
 
 	// views for background
-	sf::View m_BGMainView;
-	sf::View m_BGLeftView;
-	sf::View m_BGRightView;
+	View m_BGMainView;
+	View m_BGLeftView;
+	View m_BGRightView;
 
-	sf::View m_HudView;
+	View m_HudView;
 
+	// sprite and texture for the background
+	Sprite m_BGSprite;
+	Texture m_BGTexture;
 
 // private variables
 private:
@@ -32,11 +37,34 @@ private:
 	// The force pushing the characters down
 	const int GRAVITY = 300;
 
+	// Bool variables
+	// is game playeed
+	bool m_Playing = false;
+
+	// wich charachter is in focus
+	bool m_Char_1 = true;
+
+	// Start in full screen (not split) mode
+	bool m_SplitScreen = false;
+
+	// is it time to launch a new level
+	bool m_NewLvl = false;
+
+	// Time left in the current level (seconds)
+	float m_TimeRemaining = 10;
+	Time m_GameTimeTotal;
+
 // private functions
+private:
+	void m_input();
+	void m_update(float dtAsSeconds);
+	void m_draw();
 
 // public functions
 public:
 	Engine();
 
+	// run will call all private functions
+	void run();
 };
 
