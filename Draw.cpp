545e51fa5b10f44fed8073d3533ea1ee2e2b4ab0
DanilLvov <danillvov2002@gmail.com>
2020-01-04 15:38:27 +0200

    Commit 5: Added declaration for input, update and draw functions

---
+++ Draw.cpp
@@ -0,0 +1,46 @@
+#include "Engine.h"
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+void Engine::m_draw()
+{
+	// clear previous frame
+	m_Window.clear(Color::White);
+
+	if (!m_SplitScreen)
+	{
+		// Switch to background view
+		m_Window.setView(m_BGMainView);
+		// Draw the background
+		m_Window.draw(m_BGSprite);
+		// Switch to m_MainView
+		m_Window.setView(m_MainView);
+	}
+	else
+	{
+		// split screen is activated
+		// drawing left side of the screen
+
+		// Switch to background view
+		m_Window.setView(m_BGLeftView);
+		// Draw the background
+		m_Window.draw(m_BGSprite);
+		// Switch to m_MainView
+		m_Window.setView(m_LeftView);
+
+		// drawing right side of the screen
+
+		// Switch to background view
+		m_Window.setView(m_BGRightView);
+		// Draw the background
+		m_Window.draw(m_BGSprite);
+		// Switch to m_MainView
+		m_Window.setView(m_RightView);
+	}
+
+	// switching to Hud
+	m_Window.setView(m_HudView);
+
+	m_Window.display();
+}
---
+++ Input.cpp
@@ -0,0 +1,34 @@
+#include "Engine.h"
+#include <SFML/Graphics.hpp>
+
+void Engine::m_input()
+{
+	Event event;
+
+	while (m_Window.pollEvent(event))
+	{
+		if (event.type == Event::KeyPressed)
+		{
+			// Handling quiting
+			if (Keyboard::isKeyPressed(Keyboard::Escape))
+			{
+				m_Window.close();
+			}
+			// Handle the player starting the game
+			if (Keyboard::isKeyPressed(Keyboard::Return))
+			{
+				m_Playing = true;
+			}
+			// Switch between Thomas and Bob
+			if (Keyboard::isKeyPressed(Keyboard::Q))
+			{
+				m_Char_1 = !m_Char_1;
+			}
+			// Switch between full and split-screen
+			if (Keyboard::isKeyPressed(Keyboard::E))
+			{
+				m_SplitScreen = !m_SplitScreen;
+			}
+		}
+	}
+}
